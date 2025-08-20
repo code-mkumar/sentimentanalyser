43d086f6523bd1e5a01f73d57bfb3337fe14c09b
mukesh <22us25@anjaconline.org>
2025-08-20 21:36:11 +0530

    first commit

--- app.py
+++ app.py
@@ -32,7 +32,7 @@ if st.button("Analyze"):
         # Collect Tweets
         with st.spinner("Fetching tweets..."):
             tweets = client.search_recent_tweets(query=query,
-                                                 max_results=100,
+                                                 max_results=50,
                                                  tweet_fields=["created_at", "text"])
 
         if not tweets.data:
